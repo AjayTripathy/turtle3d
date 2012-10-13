819fcdda9b7c5a99bcb81b70ab98e311ddadd27a
Ajay Tripathy <aurotripathy@Mackenzie.local>
2012-10-13 13:15:07 -0700

    added remote link

--- static/js/codetest.js
+++ static/js/codetest.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $('#codesubmitbutton').click(function() {
         console.log("clicked");
-        $.getJSON('http://localhost:8081?jsoncallback=?', {code:$('#codefield').val()}, function(data) {
+        //$.getJSON('http://localhost:8081?jsoncallback=?', {code:$('#codefield').val()}, function(data) {
+        $.getJSON('http://sridattalabs.com:8081?jsoncallback=?', {code:$('#codefield').val()}, function(data) {
             console.log(data);
             Exec(data)
         })  
