3c90bcdf7123be59015b514fa5b99ed48139b3f4
Ross Breytberg <rossbreytberg@gmail.com>
2012-10-12 20:36:15 -0700

    stuff

--- static/js/index.js
+++ static/js/index.js
@@ -17,7 +17,7 @@ window.onload = function() {
     /////////////
     // graphics
     ///////////
-    var CAMERA_RADIUS = 200,
+    var CAMERA_DISTANCE = 200,
         CAMERA_ROTATION_H = 0,
         CAMERA_ROTATION_V = 0;
 
@@ -30,11 +30,19 @@ window.onload = function() {
 
     var renderer = new THREE.WebGLRenderer();
     var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = CAMERA_RADIUS;
+    camera.position.z = CAMERA_DISTANCE;
     var scene = new THREE.Scene();
     scene.add(camera);
     renderer.setSize(width, height);
     document.body.appendChild(renderer.domElement);
+
+    var turtleGeometry = new THREE.CubeGeometry(50, 50, 50);
+    var turtleMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: false });
+    var turtle = new THREE.Mesh(turtleGeometry, turtleMaterial);
+    var turtleX = turtle.position.x;
+    var turtleY = turtle.position.y;
+    var turtleZ = turtle.position.z;
+    scene.add(turtle);
 
 
     var drawLine = function(x0, y0, z0, x1, y1, z1, color) {
@@ -42,31 +50,46 @@ window.onload = function() {
             color = 0x000000;
         }
         var geometry = new THREE.Geometry();
-        geometry.vertices.push( new THREE.Vector3(x0, y0, z0) );
-        geometry.vertices.push( new THREE.Vector3(x1, y1, z1) );
-        var material = new THREE.LineBasicMaterial( { color: color } );
-        var mesh = new THREE.Line( geometry, material );
-        scene.add( mesh );
-        renderer.render(scene, camera);
+        geometry.vertices.push(new THREE.Vector3(x0, y0, z0));
+        geometry.vertices.push(new THREE.Vector3(x1, y1, z1));
+        var material = new THREE.LineBasicMaterial({ color: color });
+        var line = new THREE.Line(geometry, material);
+        scene.add(line);
+        return line;
     };
 
     var rotateCameraTo = function(hdegrees, vdegrees) {
         var hradians = Math.PI * hdegrees/180;
         var vradians = Math.PI * vdegrees/180;
         var x, y, z;
-        y = CAMERA_RADIUS * Math.sin(vradians);
-        z = CAMERA_RADIUS * Math.cos(vradians);
+        y = CAMERA_DISTANCE * Math.sin(vradians);
+        z = CAMERA_DISTANCE * Math.cos(vradians);
         x = z * Math.sin(hradians);
         z = z * Math.cos(hradians);
         camera.position.x = x;
         camera.position.y = y;
         camera.position.z = z;
         camera.lookAt(new THREE.Vector3(0,0,0));
-        renderer.render(scene, camera);
+    };
+
+    var updateCameraDistance = function() {
+        
     };
 
     var moveTurtleTo = function(x, y, z) {
+        turtle.position = new THREE.Vector3(x, y, z);
+    };
 
+    var moveTurtleTo = function(x, y, z) {
+        turtleX = x;
+        turtleY = y;
+        turtleZ = z;
+    };
+
+    var animate = function() {
+        requestAnimationFrame(animate);
+        if (turtle.position.)
+        renderer.render(scene, camera);
     };
 
 
@@ -91,6 +114,11 @@ window.onload = function() {
         CAMERA_ROTATION_V = Math.min(CAMERA_ROTATION_V, 90);
         CAMERA_ROTATION_V = Math.max(CAMERA_ROTATION_V, -90);
         rotateCameraTo(CAMERA_ROTATION_H, CAMERA_ROTATION_V);
+    });
+
+    document.addEventListener('scrollwheel', function(evt) {
+        CAMERA_DISTANCE += evt.wheelDeltaY;
+        updateCameraDistance();
     });
 
     // testing stuff
