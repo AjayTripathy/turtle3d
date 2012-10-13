fba12c7dc2b3339981d22f7dcacd3abbb690c0ea
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-10-12 20:38:41 -0700

    move event listeners to listeners.js

---
+++ static/js/listeners.js
@@ -0,0 +1,29 @@
+/////////////
+// graphics controls
+///////////
+document.addEventListener('keydown', function(evt) {
+    switch (evt.keyCode) {
+        case 37: // left arrow
+            CAMERA_ROTATION_H += 5;
+            break;
+        case 38: // up arrow
+            CAMERA_ROTATION_V -= 5;
+            break;
+        case 39: // right arrow
+            CAMERA_ROTATION_H -= 5;
+            break;
+        case 40: // down arrow
+            CAMERA_ROTATION_V += 5;
+            break;
+    }
+    CAMERA_ROTATION_V = Math.min(CAMERA_ROTATION_V, 90);
+    CAMERA_ROTATION_V = Math.max(CAMERA_ROTATION_V, -90);
+    rotateCameraTo(CAMERA_ROTATION_H, CAMERA_ROTATION_V);
+});
+
+document.addEventListener('scrollwheel', function(evt) {
+    CAMERA_DISTANCE += evt.wheelDeltaY;
+    updateCameraDistance();
+});
+
+
